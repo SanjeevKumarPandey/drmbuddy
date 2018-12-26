ab56ec02ff842292a1164950157f2c348eb0ea9b
Sanjeev Pandey <sanpande@adobe.com>
2018-12-26 18:18:30 +0530

    Added Status Messages with boolean

--- drmbuddy.js
+++ drmbuddy.js
@@ -1,16 +1,53 @@
 var statuses = [];
+var __msg = "";
+var msg = [];
+
+//index 0 - license svr 
+//index 1 - indiv svr
+//index 2 - crl svr
+//license_svr","License
 
 $(function() {
-    if (readCookie("status") != null) {
+    if ((readCookie("status") != null) && (readCookie("message") != null)) {
         //$("body").removeClass().addClass(readCookie("color"));
         var getCookie = readCookie("status");
+        var getMsg = readCookie("message");
         var currStatus = JSON.parse(getCookie);
+        var _msg = JSON.parse(getMsg);
         console.log('cookie found\n');
         console.log(currStatus);
-        if (currStatus[0] == true)
-        document.getElementById(id).innerHTML= '<span style="font-weight:700; font-size:80px;">OK</span>';
-        document.getElementById(widget_id).classList.remove("bad");
-        document.getElementById(widget_id).classList.add("ok");
+        for(i=0; i<3; i++){
+            if (currStatus[i]==true){
+                if(i===0){
+                    document.getElementById('License').innerHTML= `<span style="font-weight:700; font-size:80px;">${_msg[0]}</span>`;
+                    document.getElementById('license_svr').classList.remove("bad");
+                    document.getElementById('license_svr').classList.add("ok");
+                } else if(i===1){
+                    document.getElementById('Indiv').innerHTML= `<span style="font-weight:700; font-size:80px;">${_msg[1]}</span>`;
+                    document.getElementById('indiv_svr').classList.remove("bad");
+                    document.getElementById('indiv_svr').classList.add("ok");
+                } else if(i===2){
+                    document.getElementById('CRL').innerHTML= `<span style="font-weight:700; font-size:80px;">${_msg[2]}</span>`;
+                    document.getElementById('crl_svr').classList.remove("bad");
+                    document.getElementById('crl_svr').classList.add("ok");
+                }
+
+            } else if (currStatus[i]==false){
+                if(i===0){
+                    document.getElementById('license_svr').classList.remove("ok");
+                    document.getElementById('license_svr').classList.add("bad");
+                    document.getElementById('License').innerHTML= `<span style="font-weight:600; font-size:35px;">${_msg[0]}</span>`;
+                } else if(i===1){
+                    document.getElementById('indiv_svr').classList.remove("ok");
+                    document.getElementById('indiv_svr').classList.add("bad");
+                    document.getElementById('Indiv').innerHTML= `<span style="font-weight:600; font-size:35px;">${_msg[1]}</span>`;
+                } else if(i===2){
+                    document.getElementById('crl_svr').classList.remove("ok");
+                    document.getElementById('crl_svr').classList.add("bad");
+                    document.getElementById('CRL').innerHTML= `<span style="font-weight:600; font-size:35px;">${_msg[2]}</span>`;
+                }
+            }
+        }
     } else {
         console.log('cookie NOT found');
     }
@@ -35,6 +72,8 @@ function externalURLHandler(url, widget_id, id, status, index){
         document.getElementById(widget_id).classList.add("ok");
         //$(widget_id).removeClass('bad').addClass('ok');
         status = true;
+        __msg = 'OK';
+        msg[index] = __msg;
         statuses[index] = status; 
     } else if(this.status==0) {
         //$(widget_id).removeClass('ok').addClass('bad');
@@ -42,6 +81,8 @@ function externalURLHandler(url, widget_id, id, status, index){
         document.getElementById(widget_id).classList.add("bad");
         document.getElementById(id).innerHTML= '<span style="font-weight:600; font-size:35px;">No Internet!</span>';
         status = false;
+        __msg = 'No Internet!';
+        msg[index] = __msg;
         statuses[index] = status; 
     } else if(this.status==404){
         //$(widget_id).removeClass('ok').addClass('bad');
@@ -49,6 +90,8 @@ function externalURLHandler(url, widget_id, id, status, index){
         document.getElementById(widget_id).classList.add("bad");
         document.getElementById(id).innerHTML= '<span style="font-weight:700; font-size:75px;">404</span>';
         status = false;
+        __msg = '404';
+        msg[index] = __msg;
         statuses[index] = status;
     } else {
         //$(widget_id).removeClass('ok').addClass('bad');
@@ -56,6 +99,8 @@ function externalURLHandler(url, widget_id, id, status, index){
         document.getElementById(widget_id).classList.add("bad");
         document.getElementById(id).innerHTML= `ERROR! ${this.status}`;
         status = false;
+        __msg = 'ERROR';
+        msg[index] = __msg;
         statuses[index] = status;
     }
     };};
@@ -90,6 +135,7 @@ externalURLHandler("http://crl2.adobe.com/Adobe/FlashAccessIndividualizationCA.c
 //statuses.join('|');
 //JSON.stringify(statuses)
 createCookie("status", JSON.stringify(statuses));
+createCookie("message", JSON.stringify(msg));
 }
 
 function createCookie(name,value,days) {
